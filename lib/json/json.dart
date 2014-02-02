dc4424eb99abe84d8523a644b75d0c9940bf20dc
Steven Roose <stevenroose@gmail.com>
2014-02-02 19:27:21 +0100

    started json implementation

---
+++ lib/json/json.dart
@@ -0,0 +1,15 @@
+library dartcoin.json;
+
+
+import "package:dartcoin/dartcoin.dart";
+
+import "dart:typed_data";
+import "dart:collection";
+import "package:json/json.dart";
+import "package:json_rpc/json_rpc.dart";
+
+part "../src/json/json_encoder.dart";
+
+// JSON-RPC
+
+part "../src/json/json-rpc/bitcoin_api.dart";
